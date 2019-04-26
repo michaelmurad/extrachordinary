89f5d3f76174aef1caae89b30408b0adb5ef780c
Michael Murad <neverthegreat@icloud.com>
2019-04-26 12:45:02 -0700

    Remove becuase useless

--- src/chordTouchHandle.ts
+++
@@ -1,7 +0,0 @@
-// plays chord on touch
-export const chordTouchHandle = (e: any, playChord: any) => {
-  if (e && e.target && e.target.innerHTML) {
-    // return playChord(e.target.innerHTML, notes, synth);
-    return playChord;
-  }
-};
