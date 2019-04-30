cc400cbd133b2b22ee8719213c6fd614644c325a
Michael Murad <neverthegreat@icloud.com>
2019-04-30 13:54:43 -0700

    added ncp

--- scripts/moveScript.js
+++ scripts/moveScript.js
@@ -1,11 +1,25 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.copyFile('public/index.html', 'dist/index.html', err => {
-  if (err) throw err;
-  console.log('public/index.html was copied to dist/index.html');
-});
+// fs.copyFile('public/index.html', 'dist/index.html', err => {
+//   if (err) throw err;
+//   console.log('public/index.html was copied to dist/index.html');
+// });
 
-fs.copyFile('public/styles.css', 'dist/styles.css', err => {
-  if (err) throw err;
-  console.log('public/styles.css was copied to dist/styles.css');
+// fs.copyFile('public/styles.css', 'dist/styles.css', err => {
+//   if (err) throw err;
+//   console.log('public/styles.css was copied to dist/styles.css');
+// });
+
+const ncp = require('ncp').ncp;
+
+ncp.limit = 16;
+
+const source = 'public';
+const destination = 'dist';
+
+ncp(source, destination, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('done!');
 });
