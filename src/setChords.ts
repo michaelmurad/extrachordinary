3670e00a22106e3d2b925dfb6bd35900ecccf669
Michael Murad <neverthegreat@icloud.com>
2019-07-19 12:39:26 -0700

    moved Const lines to top

--- src/setChords.ts
+++ src/setChords.ts
@@ -20,49 +20,53 @@ export const setChordalIntervals = (
   const { notes, scales, chordTouchHandle } = setChordalIntervalsObject;
   const currentScaleNotes = setNewScale(root, scale, scales, notes);
   const chordsExist = document.getElementsByClassName('chords');
+
   if (chordsExist.length) {
     clearChords();
   }
   // this is an array of all the chords in the scale
   const chords = createChords(currentScaleNotes);
-  // find tonic
+
+  // This is a base chord node that will be cloned
+  const chordNode: HTMLElement = document.createElement('span');
+  chordNode.className = 'chords';
+
+  const tonicI = chordNode.cloneNode(true) as HTMLElement;
+  const tonicIII = chordNode.cloneNode(true) as HTMLElement;
+  const tonicVI = chordNode.cloneNode(true) as HTMLElement;
+  const preDomII = chordNode.cloneNode(true) as HTMLElement;
+  const preDomIV = chordNode.cloneNode(true) as HTMLElement;
+  const domV = chordNode.cloneNode(true) as HTMLElement;
+  const domVII = chordNode.cloneNode(true) as HTMLElement;
+
   const tonicEle = document.getElementById('tonics');
-  // set tonic I III VI
-  const span1: HTMLElement = document.createElement('span');
-  span1.className = 'chords';
-
-  const span2 = span1.cloneNode(true) as HTMLElement;
-  const span3 = span1.cloneNode(true) as HTMLElement;
-  span1.innerHTML = findChord(chords[0], notes);
-  span2.innerHTML = findChord(chords[2], notes);
-  span3.innerHTML = findChord(chords[5], notes);
-  tonicEle && tonicEle.appendChild(span1);
-  tonicEle && tonicEle.appendChild(span2);
-  tonicEle && tonicEle.appendChild(span3);
-  // find predominant
   const predominantEl = document.getElementById('predominant');
-  // set pre-dominant II IV
-  const preDom2 = span1.cloneNode() as HTMLElement;
-  const preDom4 = span1.cloneNode() as HTMLElement;
-  preDom2.innerHTML = findChord(chords[1], notes);
-  preDom4.innerHTML = findChord(chords[3], notes);
-  predominantEl && predominantEl.appendChild(preDom2);
-  predominantEl && predominantEl.appendChild(preDom4);
-  // find dominant
   const dominantEl = document.getElementById('dominant');
-  // // set dominant V VII
-  const dom5 = span1.cloneNode() as HTMLElement;
-  const dom7 = span1.cloneNode() as HTMLElement;
-  dom5.innerHTML = findChord(chords[4], notes);
-  dom7.innerHTML = findChord(chords[6], notes);
 
-  dominantEl && dominantEl.appendChild(dom5);
-  dominantEl && dominantEl.appendChild(dom7);
+  // set tonic I III VI
+  tonicI.innerHTML = findChord(chords[0], notes);
+  tonicIII.innerHTML = findChord(chords[2], notes);
+  tonicVI.innerHTML = findChord(chords[5], notes);
+  tonicEle && tonicEle.appendChild(tonicI);
+  tonicEle && tonicEle.appendChild(tonicIII);
+  tonicEle && tonicEle.appendChild(tonicVI);
+
+  // set pre-dominant II IV
+  preDomII.innerHTML = findChord(chords[1], notes);
+  preDomIV.innerHTML = findChord(chords[3], notes);
+  predominantEl && predominantEl.appendChild(preDomII);
+  predominantEl && predominantEl.appendChild(preDomIV);
+
+  // set dominant V VII
+  domV.innerHTML = findChord(chords[4], notes);
+  domVII.innerHTML = findChord(chords[6], notes);
+  dominantEl && dominantEl.appendChild(domV);
+  dominantEl && dominantEl.appendChild(domVII);
+
   // // set secondary dominant
   createSecondaryDominants(currentScaleNotes, notes);
-  const chordClass = document.getElementsByClassName('chords');
   // adds event listener to each node to play chord sound with synth
-  [...chordClass].forEach(element =>
+  [...document.getElementsByClassName('chords')].forEach(element =>
     element.addEventListener('click', chordTouchHandle)
   );
 };
